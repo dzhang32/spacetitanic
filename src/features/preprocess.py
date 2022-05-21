d44bed04d0232b452e4de5594f13d7efcb6b1e8c
David Zhang <dyzhang32@gmail.com>
2022-05-21 10:13:22 +0100

    feat(tidy_cabin): add function

---
+++ src/features/preprocess.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def tidy_cabin(df: pd.DataFrame) -> pd.DataFrame:
+    """Split cabin into deck, cabin_num and side.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Contains the cabin variable.
+
+    Returns
+    -------
+    pd.DataFrame
+        Contains the deck, cabin_num and side, with the original cabin variable
+        dropped.
+    """
+    df[["deck", "cabin_num", "side"]] = df["Cabin"].str.split("/", 2, expand=True)
+    df = df.drop(columns="Cabin")
+
+    return df
